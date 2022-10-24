9b7b32b494b24f9fd23436767d6075334cb997be
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-24 13:03:24 -0400

    Separated cmd into module

---
+++ src/cmd/config.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::{Args, Subcommand};
+
+use crate::config::ConfigOpts;
+
+/// Configuration controls
+#[derive(Debug, Args)]
+#[clap(name = "config")]
+pub struct Config {
+    #[clap(subcommand)]
+    action: ConfigSubcommands,
+}
+
+impl Config {
+    pub async fn run(self, config: Option<PathBuf>) -> Result<()> {
+        match self.action {
+            ConfigSubcommands::Show => {
+                let cfg = ConfigOpts::full(config)?;
+                println!("{:#?}", cfg);
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum ConfigSubcommands {
+    /// Show CFDDNS's current config pre-CLI.
+    Show,
+}
