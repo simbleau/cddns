62ffc1b265dba86c16305f4ba9eae64545fedde7
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-25 12:24:49 -0400

    Added verify command

---
+++ src/cmd/verify.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use crate::{
+    cloudfare,
+    config::{ConfigOpts, ConfigOptsVerify},
+};
+use anyhow::Result;
+use clap::Args;
+
+/// Verify authentication
+#[derive(Debug, Args)]
+#[clap(name = "")]
+pub struct Verify {
+    #[clap(flatten)]
+    pub cfg: ConfigOptsVerify,
+}
+
+impl Verify {
+    pub async fn run(self, config: Option<PathBuf>) -> Result<()> {
+        let toml_cfg = ConfigOpts::from_file(config)?;
+        let env_cfg = ConfigOpts::from_env()?;
+        let cli_cfg = ConfigOpts {
+            verify: Some(self.cfg),
+            ..Default::default()
+        };
+        // Apply layering to configuration data (TOML < ENV < CLI)
+        let opts = toml_cfg.merge(env_cfg).merge(cli_cfg);
+
+        println!("{:#?}", opts);
+        let verify_opts = opts.verify.unwrap_or_default();
+        let token = match verify_opts.token {
+            Some(t) => t,
+            None => anyhow::bail!("no token was provided"),
+        };
+        cloudfare::endpoints::verify(&token).await?;
+        Ok(())
+    }
+}
