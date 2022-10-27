8d6185577c8eef9d52b22fba1d0a58dd042114f0
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-26 20:39:31 -0400

    Removed serialize trait on models

--- src/cloudfare/models.rs
+++ src/cloudfare/models.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct VerifyResponse {
     pub success: bool,
 }
