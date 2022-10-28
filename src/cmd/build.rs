17693b3d755041154c9c6f42e3aa8f927a2c5593
Spencer C. Imbleau <spencer@imbleau.com>
2022-10-28 11:22:20 -0400

    Simplified save pipeline

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -1,7 +1,11 @@
 use crate::config::{ConfigOpts, ConfigOptsVerify};
 use anyhow::{Context, Result};
 use clap::{Args, Subcommand};
-use std::{io::Write, path::Path};
+use std::{
+    ffi::OsString,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
 /// Build configuration or inventory files
 #[derive(Debug, Args)]
@@ -73,26 +77,38 @@ async fn prompt(
     Ok(input.context("aborted")?)
 }
 
-async fn build_config(
+async fn save<T, P>(
+    contents: &T,
+    default_path: P,
     receiver: &mut tokio::sync::mpsc::Receiver<String>,
-) -> Result<()> {
-    // Build config
-    let token = prompt("Cloudfare API token", receiver).await?;
-    let config = ConfigOpts {
-        verify: Some(ConfigOptsVerify { token: Some(token) }),
-        list: None,
-    };
-
+) -> Result<()>
+where
+    T: ?Sized + serde::Serialize,
+    P: AsRef<Path>,
+{
+    let name = default_path
+        .as_ref()
+        .file_name()
+        .expect("invalid default path file name");
+    let ext = default_path
+        .as_ref()
+        .extension()
+        .expect("invalid default file extension");
     // Get output path
-    let mut output_path =
-        prompt("Output location [default: CFDDNS.toml]", receiver).await?;
+    let mut output_path = OsString::from(
+        prompt(
+            &format!(
+                "Output path [default: {}]",
+                default_path.as_ref().display()
+            ),
+            receiver,
+        )
+        .await?,
+    );
     if output_path.is_empty() {
-        output_path.push_str("CFDDNS");
+        output_path.push(name);
     }
-    if !output_path.ends_with(".toml") {
-        output_path.push_str(".toml");
-    }
-    let output_path = Path::new(&output_path);
+    let output_path = &PathBuf::from(&output_path).with_extension(ext);
     if output_path.exists() {
         match prompt("File location exists, overwrite? (y/N)", receiver)
             .await?
@@ -109,11 +125,24 @@ async fn build_config(
     // Save
     tokio::fs::write(
         output_path,
-        toml::to_string(&config).context("error encoding config to TOML")?,
+        toml::to_string(&contents).context("error encoding config to TOML")?,
     )
     .await
-    .context("error saving config")?;
+    .context("error saving contents")?;
+    Ok(())
+}
 
+async fn build_config(
+    receiver: &mut tokio::sync::mpsc::Receiver<String>,
+) -> Result<()> {
+    // Build config
+    let token = prompt("Cloudfare API token", receiver).await?;
+    let config = ConfigOpts {
+        verify: Some(ConfigOptsVerify { token: Some(token) }),
+        list: None,
+    };
+
+    save(&config, "CFDDNS.toml", receiver).await?;
     println!("Saved");
     Ok(())
 }
