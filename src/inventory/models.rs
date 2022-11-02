34c20d45f47b1e5f13badb5a6ce73ad4300c53ab
Spencer C. Imbleau <spencer@imbleau.com>
2022-11-01 20:44:46 -0400

    Implemented iterator for Inventory model

--- src/inventory/models.rs
+++ src/inventory/models.rs
@@ -39,3 +39,49 @@ impl Inventory {
         Ok(cfg)
     }
 }
+
+/// An iterator over the zone and corresponding records
+pub struct InventoryIter {
+    items: Vec<(String, Vec<String>)>,
+    curr: usize,
+}
+
+impl Iterator for InventoryIter {
+    /// A tuple containing the zone ID and a list of child record IDs
+    type Item = (String, Vec<String>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.curr;
+        if current < self.items.len() {
+            self.curr += 1;
+            let (zone, records) = &self.items[current];
+            Some((zone.clone(), records.clone()))
+        } else {
+            None
+        }
+    }
+}
+
+impl IntoIterator for Inventory {
+    /// A tuple containing the zone ID and a list of child record IDs
+    type Item = (String, Vec<String>);
+    type IntoIter = InventoryIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let mut items = HashMap::new();
+        if let Some(map) = self.0 {
+            for (key, value) in map {
+                items.entry(key.to_owned()).or_insert(Vec::new());
+                if let Some(record_set) = value.0 {
+                    for record in record_set {
+                        items.get_mut(&key).unwrap().push(record.0.clone());
+                    }
+                }
+            }
+        }
+        InventoryIter {
+            items: Vec::from_iter(items),
+            curr: 0,
+        }
+    }
+}
