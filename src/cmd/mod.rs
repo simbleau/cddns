0cc92f46657bfdacee6f4b36cf164383597833b5
Spencer C. Imbleau <simbleau@blueorigin.com>
2022-11-26 12:49:59 -0500

    Modified access to command modules

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,13 +1,6 @@
 //! Clap commands handled by the CLI.
 
-mod config;
-pub use config::ConfigCmd;
-
-mod inventory;
-pub use inventory::InventoryCmd;
-
-mod list;
-pub use list::ListCmd;
-
-mod verify;
-pub use verify::VerifyCmd;
+pub mod config;
+pub mod inventory;
+pub mod list;
+pub mod verify;
